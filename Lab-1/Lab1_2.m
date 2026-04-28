8171aa735630333cf8e0a8ea66b4df6922e82d18
Nitun Krishna Biswas <nkbiswas05@gmail.com>
2026-04-28 22:23:27 +0600

    Add matrix operations in Lab1_2.m

---
+++ Lab-1/Lab1_2.m
@@ -0,0 +1,22 @@
+A = [1 2 3; 4 5 6];
+B = [6 5 4; 3 2 1];
+disp("A=")
+disp(A)
+disp("B=")
+disp(B)
+add_result = A + B;
+
+sub_result = A - B;
+
+mul_result = A .* B;
+
+div_result = A ./ B;
+
+disp("A+B=")
+disp(add_result);
+disp("A-B=")
+disp(sub_result);
+disp("A*B=")
+disp(mul_result);
+disp("A/B=")
+disp(div_result);
